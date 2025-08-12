6a1e9cc1a30d3725373ab29cfa9ca13d5b218cf1
Pedro <rm16387@fiap.com.br>
2025-08-12 12:07:07 -0300

    Update index.js

--- src_intf/js/index.js
+++ src_intf/js/index.js
@@ -3,8 +3,14 @@ qntAbrirAdmin = 0
 function abrirAdmin() {
     qntAbrirAdmin++
 
-    if (qntAbrirAdmin === 10) {
+    if (qntAbrirAdmin >= 10) {
         qntAbrirAdmin = 0
-        window.location.replace('painelAdmin.html')
+        senhaInserida = prompt("Digite a senha de administrador")
+
+        if (senhaInserida === "123"){
+            window.location.replace('painelAdmin.html')
+        } else {
+            alert("Senha incorreta!")
+        }
     }
 }
