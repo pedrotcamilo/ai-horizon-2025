05b7a3302867ef0a733ec8f7304ca270c1318fa8
Pedro <rm16387@fiap.com.br>
2025-11-04 12:40:16 -0300

    asdas

--- backend/templates/js/imaginacaoResultado.js
+++ backend/templates/js/imaginacaoResultado.js
@@ -1,1 +1,12 @@
+// texto generico só pra funcionar
+
+fetch("/api/receberMundoPerfeito", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+        mundo_perfeito: 'Imagine um mundo perfeito com a paz mundial'
+    }),
+});
+
 fetch('/api/elevarEtapa')
+location.reload()
